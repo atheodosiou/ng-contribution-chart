c1e17ec8285dd2a09dc471a1501d8afe14668b7d
Anastasios Theodosiou <anastasios.theodosiou@gmail.com>
2019-08-19 16:57:30 +0300

    adds

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-contribution-chart';
+  totalWeeks = 0;
+  totalDays = 0;
+  ngOnInit() {
+    this.totalDays = this.getTotalDaysOfYear(2013);
+    this.totalWeeks = 52;
+  }
+
+  private isLeapYear(year: number) {
+    return year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0);
+  }
+
+  private getTotalDaysOfYear(year: number) {
+    return this.isLeapYear(year) ? 366 : 365;
+  }
 }
